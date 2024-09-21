29ed9ae57af277b7e19df9fa2234decf08b4be44
Christian Westrom <c.westrom@westrom.xyz>
2024-09-21 18:32:32 +0900

    just gimme a flake man

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "very minimal flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        legacyPackages = pkgs;
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            just
+            fasm
+          ];
+        };
+      }
+    );
+}
